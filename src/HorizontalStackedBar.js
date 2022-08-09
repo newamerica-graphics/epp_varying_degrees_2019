16c56ba62411afe1accd238f5cb4756a676a75b4
nmorduch <1051450+nmorduch@users.noreply.github.com>
2022-08-09 14:59:46 -0400

    Bold correct rows

--- src/HorizontalStackedBar.js
+++ src/HorizontalStackedBar.js
@@ -145,7 +145,7 @@ const HorizontalStackedBar = ({
           textAnchor: "end",
           verticalAnchor: "middle",
           fontSize: 14,
-          fontWeight: (d == "2022" || d == "Total") && "bold", // TODO don't hard-code this text
+          fontWeight: (d == "Have Student Loans" || d == "Don't Have Student Loans") && "bold", // TODO don't hard-code this text
           dy: d == "Total" && - total_bar_offset
         })}
       />
